f887b4e1837ca15e067203822558bee340d1f528
hanajimajo <hanakun0318@icloud.com>
2021-05-28 16:59:54 +0000

    layout update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,15 +17,23 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to articles_path
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
   private
